d376d5052249407de542feb751e243f85e0c4074
ivanfr26 <ivanfr26@gmail.com>
2012-09-01 20:44:32 -0300

    sad

--- Debug/controller/libs/subdir.mk
+++ Debug/controller/libs/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 controller/libs/%.o: ../controller/libs/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/usr/include -I/usr/lib/gcc/x86_64-linux-gnu/4.4.3/include -I/usr/lib/gcc/x86_64-linux-gnu/4.4/include-fixed -I/usr/local/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
