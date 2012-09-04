77b78b543eab5cada0739706bbbe9b877dbad945
ivanfr26 <ivanfr26@gmail.com>
2012-09-04 19:16:37 -0300

    sad

--- Debug/controller/libs/socketTools.d
+++
@@ -1,4 +0,0 @@
-controller/libs/socketTools.d controller/libs/socketTools.o: \
- ../controller/libs/socketTools.c ../controller/libs/socketTools.h
-
-../controller/libs/socketTools.h:
--- Debug/controller/webclient.d
+++
@@ -1,4 +0,0 @@
-controller/webclient.d controller/webclient.o: ../controller/webclient.c \
- ../controller/libs/socketTools.h
-
-../controller/libs/socketTools.h:
